473d7f0cc3b9301bf9fbd3898fd379c27d1eb74b
Some HDL-Guy <s@h.g>
2013-11-18 20:51:14 -0800

    software/fpga/whacker/utils: fix potential ordering issue

--- software/fpga/ov3/whacker/util.py
+++ software/fpga/ov3/whacker/util.py
@@ -9,19 +9,20 @@ def dmatpl(depth):
 class Acc(Module):
     def __init__(self, *args, **kwargs):
         self.v = Signal(*args, **kwargs)
-        self.n = Signal(*args, **kwargs)
 
-        self.comb += self.n.eq(self.v)
-        self.sync += self.v.eq(self.n)
+        self._n = Signal(*args, **kwargs)
+        self._s = Signal(1)
+
+        self.sync += If(self._s, self.v.eq(self._n))
 
     def set(self, val):
-        return self.n.eq(val)
+        return self._n.eq(val), self._s.eq(1)
 
 class Acc_inc(Acc):
     def inc(self):
-        return self.n.eq(self.v+1)
+        return self._n.eq(self.v+1), self._s.eq(1)
 
 class Acc_or(Acc):
     def _or(self, v):
-        return self.n.eq(self.v | v)
+        return self._n.eq(self.v | v), self._s.eq(1)
 
